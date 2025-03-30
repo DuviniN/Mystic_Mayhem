dca84a13dd0f506fa46aa09c4f262734cf5afe1e
De Silva N. D. N. <desilva-se22023@stu.kln.ac.lk>
2025-03-30 10:14:35 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,26 @@ public class Main {
     public static void main(String[] args) {
 
         DisplayController displayController =new DisplayController();
-        GameController gameControl=new GameController();
+        GameController gameController =new GameController();
         final Scanner input =new Scanner(System.in);
         int userChoice= displayController.loginPage();
 
         User user;
         if(userChoice==1){
             displayController.clearConsole();
-            user=gameControl.login();
+            user= gameController.login();
         }
         else{
             displayController.clearConsole();
-            user=gameControl.register();
+            user= gameController.register();
         }
-        user.toString();
-        displayController.displayMenu();
 
-
-
-
-
-
-
+        while (true){
+            userChoice = displayController.displayMenu();
+            switch (userChoice){
+                case 2 -> gameController.buyCharacter(user);
+                case 3 -> gameController.sellCharacter(user);
+            }
+        }
     }
 }
